9249176ab8bfb21b231c6579440bf2a57ce5e61d
snehasamuel <snehasamuel297@gmail.com>
2022-05-16 21:15:17 +0530

    Searching Student completed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Addstudent from './components/Addstudent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import Search from './components/Search';
+
 function App() {
   return (
+    
+    <>
     <Addstudent/>
+    <Search/>
+    </>
+   
   );
 }
 
