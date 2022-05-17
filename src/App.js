e41a6bfed28d250be0232199c6f7bf69da63c491
snehasamuel <snehasamuel297@gmail.com>
2022-05-17 11:25:53 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import Search from './components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     
     <>
-    <Addstudent/>
-    <Search/>
+    <BrowserRouter>
+    <Routes>
+<Route path='/' exact element={<Addstudent/>}/>
+<Route path='/search' exact element={<Search/>}/>
+
+    </Routes>
+    </BrowserRouter>
     </>
    
   );
--- src/components/Addstudent.js
+++ src/components/Addstudent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Addstudent = () => {
   return (
     <div>
+<Header/>
 
 <div className="container">
     <div className="row">
